2314569b5a6166293cae3ea8ff371554b2911f98
Dany <dany.mrtinez@gmail.com>
2019-12-10 19:28:29 +0000

    add tasks 6,7,10,11,12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for number in range(1, 101):
+        if (number % 3 != 0 and number % 5 != 0):
+            print(number, end=' ')
+        else:
+            if (number % 3 == number % 5 == 0):
+                print('FizzBuzz', end=' ')
+            elif (number % 3 == 0):
+                print('Fizz', end=' ')
+            else:
+                print('Buzz', end=' ')
