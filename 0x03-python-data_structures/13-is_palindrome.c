a06b5f84fcb77f14a26e517ea2b15f5e9f2dd130
dantsub <dany.mrtinez@gmail.com>
2019-12-17 05:34:10 +0000

    initial commit

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+  * is_palindrome - check if linked list is palindrome,
+  * @head: head.
+  *
+  * Return: 0 if it is not a palindrome, 1 if it is a palindrome.
+  */
+int is_palindrome(listint_t **head)
+{
+	if (!(*head))
+		return (1);
+	return (0);
+}
