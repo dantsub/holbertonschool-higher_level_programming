5549a2ad9f0cc5c4efbada4530f6d1a5c1b1d8a0
Dany <dany.mrtinez@gmail.com>
2019-12-17 18:47:49 +0000

    condition from or to and

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     new = my_list.copy()
-    if (len(new) > idx or idx > -1):
+    if (len(new) > idx and idx > -1):
         new[idx] = element
     return new
