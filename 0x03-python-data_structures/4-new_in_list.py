1e4c05a4ad2d79a613ef70a6be3cd548d977bff9
Dany <dany.mrtinez@gmail.com>
2019-12-17 18:44:09 +0000

    4-new_in_list.py fix?

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new = my_list.copy()
-    if (len(new) > idx or idx > 0):
-        new[idx] = element
-    return new
+    if my_list:
+        new = my_list.copy()
+        if (len(new) > idx or idx > 0):
+            new[idx] = element
+        return new
