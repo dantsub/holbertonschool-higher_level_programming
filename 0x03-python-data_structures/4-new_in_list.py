c17086ac6768e132fec7f2f48ba09c609d4aebb3
Dany <dany.mrtinez@gmail.com>
2019-12-17 17:10:00 +0000

    write code

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    new = my_list.copy()
+    if (len(new) > idx or idx > 0):
+        new[idx] = element
+    return new
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    string = ''
+    for letter in my_string:
+        if letter == 'C' or letter == 'c':
+            continue
+        string += letter
+    return string
