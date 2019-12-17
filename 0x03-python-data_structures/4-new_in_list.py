6282e7e765f10f595caf3897f7ee526c07fc4263
Dany <dany.mrtinez@gmail.com>
2019-12-17 18:45:56 +0000

    correction conditional

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if my_list:
-        new = my_list.copy()
-        if (len(new) > idx or idx > 0):
-            new[idx] = element
-        return new
+    new = my_list.copy()
+    if (len(new) > idx or idx > -1):
+        new[idx] = element
+    return new
