218843c759bfb66dd0641a04ad116a7e8f52a9ff
Dany <dany.mrtinez@gmail.com>
2019-12-18 00:31:05 +0000

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,7 +7,49 @@
   */
 int is_palindrome(listint_t **head)
 {
+	listint_t *cp_head = *head, *cp_h, *h;
+
 	if (!(*head))
 		return (1);
-	return (0);
+	h = NULL;
+	while (cp_head)
+	{
+		add_nodeint(&h, cp_head->n);
+		cp_head = cp_head->next;
+	}
+	cp_head = *head;
+	cp_h = h;
+	while (cp_head)
+	{
+		if (cp_head->n != cp_h->n)
+		{
+			free_listint(h);
+			return (0);
+		}
+		cp_head = cp_head->next;
+		cp_h = cp_h->next;
+	}
+	free_listint(h);
+	return (1);
+}
+/**
+  * add_nodeint - adds a new node at the beginning of a listint_t list.
+  * @head: node.
+  * @n: constant int.
+  * Return: the address of the new element, or NULL if it failed
+  */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	if (!(*head))
+		new->next = NULL;
+	else
+		new->next = *head;
+	*head = new;
+	return (new);
 }
