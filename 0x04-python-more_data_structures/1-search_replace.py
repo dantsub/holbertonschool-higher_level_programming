9cb70c272cdf022a6200eaa2a885985d1b914571
Dany <dany.mrtinez@gmail.com>
2019-12-19 17:41:17 +0000

    fix 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,7 +2,8 @@
 def search_replace(my_list, search, replace):
     new = []
     for element in my_list:
-        new.append(element)
         if element == search:
             new.append(replace)
+        else:
+            new.append(element)
     return new
