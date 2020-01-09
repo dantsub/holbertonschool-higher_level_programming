b8a7d580dc49c0634080c83fdd9876d0fa516e73
dantsub <dany.mrtinez@gmail.com>
2020-01-09 15:44:39 -0500

    add code 3 task

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+class Square():
+
+    """ Square class with one priveteatribute"""
+
+    def __init__(self, size=0):
+        """ __init___ constructor of Square class
+
+        Args:
+            size (int): size of Square.
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+    def area(self):
+    
+        """ Calculate area of square
+        return: area
+        """
+
+        return self.__size ** 2 
