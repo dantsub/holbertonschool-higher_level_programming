098f2a77d63287a47614acb169c700399db17c07
dantsub <dany.mrtinez@gmail.com>
2020-01-09 15:38:23 -0500

    add code

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 class Square():
+    """  Square class """
     def __init__(self, size):
+        """__init__ is a constructor of square class
+        Args:
+            size (int): size of square
+        """
         self.__size = size
         
