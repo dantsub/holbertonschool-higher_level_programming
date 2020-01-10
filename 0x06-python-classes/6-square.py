80735a892702911910d5b4486d812c1e275b3623
dantsub <dany.mrtinez@gmail.com>
2020-01-10 04:36:43 +0000

    fix 2

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -57,7 +57,7 @@ class Square():
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, int) or len(value) != 2:
+        if not isinstance(value, tuple) or len(value) != 2:
             if not isinstance(value[0], int) or not isinstance(value[1], int):
                 if value[0] < 0 or value[1] < 0:
                     raise TypeError(
