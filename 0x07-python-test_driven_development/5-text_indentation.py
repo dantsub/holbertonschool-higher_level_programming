dab93e8c012a3fa9084087d1518b4bd43faa4fc6
dantsub <dany.mrtinez@gmail.com>
2020-01-13 19:36:18 -0500

    add task 4

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,2 +1,23 @@
 #!/usr/bin/python3
+"""  """
 def text_indentation(text):
+    """
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    text = text.rstrip()
+    size = len(text)
+    idx = 0
+
+    while idx < size and ord(text[idx]) == 32:
+        idx += 1
+
+    while idx < size:
+        if text[idx] in ".?:":
+            print("{:s}".format(text[idx]), end="\n\n")
+            while (idx + 1) < size and ord(text[idx + 1]) == 32:
+                idx += 1
+        else:
+            print("{:s}".format(text[idx]), end="")
+
+        idx += 1
