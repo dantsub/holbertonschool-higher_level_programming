8f8e6a00d9607d48c5bd574339b4046605c20576
dantsub <dany.mrtinez@gmail.com>
2020-01-20 12:19:40 -0500

    add docs task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+""" Return True or False
+"""
+
+
 def inherits_from(obj, a_class):
-    """Change conditional"""
-    return True if isinstance(obj, a_class) else False
+    """ Returns True if the object is an instance of a class
+        that inherited (directly or indirectly) from the specified class;
+        otherwise False.
+    Args:
+        obj: a object
+        a_class: a Class
+        Return: True or False
+    """
+    tyob = type(obj)
+    return True if issubclass(tyob, a_class) and tyob != a_class else False
