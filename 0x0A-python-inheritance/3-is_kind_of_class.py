f959dfc9878f027e8022e103eab6e21a046a0021
dantsub <dany.mrtinez@gmail.com>
2020-01-20 12:13:26 -0500

    add docstring task 3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+""" Return True or False
+"""
+
+
 def is_kind_of_class(obj, a_class):
-    return True if isinstance(obj, a_class) else False
+        """ Returns True if the object is an instance of,
+            or if the object is an instance of a class that inherited from,
+            the specified class, otherwise False.
+        Args:
+            obj: a object.
+            a_class: a Class.
+            Return: True or False.
+        """
+        return True if isinstance(obj, a_class) else False
