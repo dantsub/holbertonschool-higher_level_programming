d7e923103e5dfd7bfd1f433a44c0000412174ae7
dantsub <dany.mrtinez@gmail.com>
2020-01-20 18:52:03 -0500

    add

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Change the operator.
+"""
+
+
+class MyInt(int):
+    """ Class MyInt """
+    
+    def __eq__(self, value):
+        return self.real != value
+    
+    def __ne__(self, value):
+        return self.real == value
