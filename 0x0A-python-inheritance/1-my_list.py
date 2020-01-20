8203577c6fb92b357df748c731be40b28073d433
dantsub <dany.mrtinez@gmail.com>
2020-01-20 11:17:06 -0500

    add docstring task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 class MyList(list):
+    """ class MyList that inherits from list """
 
     def print_sorted(self):
-        print(self.sort())
+        """ Public instance method,
+            that prints the list, but sorted (ascending sort)
+        """
+        print(sorted(self))
