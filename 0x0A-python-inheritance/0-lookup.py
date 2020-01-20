77276b4e45e225776596508dd82b5c306d7ed8b0
dantsub <dany.mrtinez@gmail.com>
2020-01-20 11:11:53 -0500

    add docstring

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+""" Return a list object.
+"""
+
+
 def lookup(obj):
-    return dir(obj)
+        """ Returns the list of available attributes and
+            methods of an objec
+        Args:
+            obj: a object
+            Return: a list object
+        """
+        return dir(obj)
