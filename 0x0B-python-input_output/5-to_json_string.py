89c301e712cce53bd784db8a0b5112d6b2ab9d1e
dantsub <dany.mrtinez@gmail.com>
2020-01-21 13:39:10 -0500

    update 5-to_json_string.py

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
+"""
+to json string module
+"""
+import json
+
+
 def to_json_string(my_obj):
+    """ Function that returns the JSON representation of an object (string)
+    
+    Args:
+        my_obj: object.
+    
+    Return:
+        JSON representation of an object
+    """
+    return json.dumps(my_obj)
