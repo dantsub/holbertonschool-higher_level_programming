46aa193e6403e59d797a8bf117f03853016d1625
dantsub <dany.mrtinez@gmail.com>
2020-01-21 07:11:37 +0000

    to improve

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 def read_lines(filename="", nb_lines=0):
+    with open(filename) as file:
+        if nb_lines < 1 or nb_lines >= sum([1 for l in file]):
+            [print(lines, end='') for lines in file]
+        else:
+            [print(file.readline(), end='') for i in range(nb_lines)]
