39b316984ef049e2be93911046f09f290b71b71e
dantsub <dany.mrtinez@gmail.com>
2020-01-21 14:45:35 -0500

    update 9-add_item.py

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
-def class_to_json(obj):
+"""
+Load, add, save module
+"""
+from sys import argv
+save_json = __import__('7-save_to_json_file').save_to_json_file
+load_json = __import__('8-load_from_json_file').load_from_json_file
+
+lis = argv[1:]
+try:
+    before = load_json('add_item.json')
+except IOError:
+    before = []
+save_json(before + lis, 'add_item.json')
