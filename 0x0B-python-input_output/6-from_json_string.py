0186da45620101a868712fef1481a861b1d5cfa8
dantsub <dany.mrtinez@gmail.com>
2020-01-21 13:45:07 -0500

    update 6-from_json_string.py

--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
+"""
+from json string module
+"""
+import json
+
+
 def from_json_string(my_str):
+    """ Function that returns an object (Python data structure)
+        represented by a JSON string
+
+    Args:
+        my_str: string.
+    """
+    return json.loads(my_str)
