28c55d20117d1a928d29b8fd005069132ec0c801
dantsub <dany.mrtinez@gmail.com>
2020-01-21 05:08:59 +0000

    delete spaces

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,9 +5,9 @@
 
 class MyInt(int):
     """ Class MyInt """
-    
+
     def __eq__(self, value):
         return self.real != value
-    
+
     def __ne__(self, value):
         return self.real == value
