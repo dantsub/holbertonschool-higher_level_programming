de0f2a3de9cdafdb7fee33c2b6f755dc4dd96834
dantsub <dany.mrtinez@gmail.com>
2020-01-21 12:07:06 -0500

    add .gitignore and update 3-write_file.py

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
+""" write file module """
+
+
 def write_file(filename="", text=""):
+    """ Function that writes a string to
+        a text file (UTF8) and returns the
+        number of characters written
+        
+    Args:
+        filename (str): filename
+        text (str): text. 
+    """
+    with open(filename, mode='w', encoding="utf-8") as file:
+        return file.write(text)
