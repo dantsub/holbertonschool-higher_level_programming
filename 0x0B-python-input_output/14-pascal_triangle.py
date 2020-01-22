ea16b4bd4f2e0ce47386e7f9bef836d050e80153
dantsub <dany.mrtinez@gmail.com>
2020-01-21 19:05:35 -0500

    update 14-pascal_triangle.py

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+Pascal triangle module
+"""
+
+
+def pascal_triangle(n):
+    """ Represent Pascal's Triangle of size n. """
+    if n <= 0:
+        return []
+
+    pascal = [[1]]
+    while len(pascal) != n:
+        tri = pascal[-1]
+        one = [1]
+        for i in range(len(tri) - 1):
+            one.append(tri[i] + tri[i + 1])
+        one.append(1)
+        pascal.append(one)
+    return pascal
