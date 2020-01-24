ae9c51499e6d43d3ed61098e82fc4486e915eb95
dantsub <dany.mrtinez@gmail.com>
2020-01-23 19:24:52 -0500

    update rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -18,30 +18,61 @@ class Rectangle(Base):
     def width(self):
         return self.__width
 
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be > 0')
+        self.__width = value
+
     @property
     def height(self):
         return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be > 0')
+        self.__height = value
 
     @property
     def x(self):
         return self.__x
 
+    @x.setter
+    def x(self, value):
+        if type(value) != int:
+            raise TypeError('x must be an integer')
+        if value < 0:
+            raise ValueError('x must be >= 0')
+        self.__x = value
+
     @property
     def y(self):
         return self.__y
 
-    @width.setter
-    def width(self, value):
-        self.__width = value
-    
-    @height.setter
-    def height(self, value):
-        self.__height = value
-
-    @x.setter
-    def x(self, value):
-        self.__x = value
-
     @y.setter
     def y(self, value):
+        if type(value) != int:
+            raise TypeError('y must be an integer')
+        if value < 0:
+            raise ValueError('y must be >= 0')
         self.__y = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def display(self):
+        if self.__y:
+            [print() for i in range(self.__y)]
+        [print('{}{}'.format(' ' * self.__x, '#' * self.__width))
+         for i in range(self.__height)]
+
+    def __str__(self):
+        string = "[Rectangle] ({}) ".format(self.id)
+        string += "{}/{} -".format(self.__x, self.__y)
+        string += " {}/{}".format(self.__width, self.__height)
+        return string
