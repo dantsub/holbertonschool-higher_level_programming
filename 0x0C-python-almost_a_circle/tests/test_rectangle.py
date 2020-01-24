3aa82b0eeef3d9b40ecf8b52592af179b64fc522
dantsub <dany.mrtinez@gmail.com>
2020-01-24 06:29:25 +0000

    add test files

---
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Test for Rectangle class
+"""
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    """ Put test"""
---
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Test for Rectangle class
+"""
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+
+class TestSquare(unittest.TestCase):
+    """ Put test"""
