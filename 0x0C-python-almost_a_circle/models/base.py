d9e37c020742e4795d1d3b687a2dec8d0141cb9a
dantsub <dany.mrtinez@gmail.com>
2020-01-24 20:27:39 -0500

    docs

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,21 +20,48 @@ class Base():
 
     @property
     def id(self):
+        """ Get id attribute """
         return self.__id
 
     @id.setter
     def id(self, value):
+
+        """ Set of id attribute
+        Args:
+
+            value (int):
+        """
         self.__id = value
 
-    @staticmethod
     def to_json_string(list_dictionaries):
+
+        """ Method that returns the JSON string
+            representation of list_dictionaries
+        Args:
+            list_dictionaries (list of dicts):
+                List of dictionaries
+
+        return: The JSON string representation
+        """
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        filename = cls.__name__+ '.json'
+
+        """ Description
+        :type cls:
+        :param cls:
+
+        :type list_objs:
+        :param list_objs:
+
+        :raises:
+
+        :rtype:
+        """
+        filename = cls.__name__ + '.json'
         with open(filename, mode='w', encoding='utf-8') as file:
             if list_objs is None:
                 file.write('[]')
@@ -44,22 +71,45 @@ class Base():
 
     @staticmethod
     def from_json_string(json_string):
+
+        """ Method that returns the list of the
+            JSON string representation json_string
+        Args:
+            json_string: a string representing a list of dictionaries
+
+        Return: JSON string representation
+        """
         if json_string is None or json_string == "[]":
             return []
         return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
+
+        """ Method that returns an instance with all attributes already set
+        Args:
+            cls (class): Rectangle or Square
+            **dictionary:
+                can be thought of as a double pointer to a dictionary
+        return: Instance
+        """
         if dictionary and dictionary is not {}:
-            if cls.__name__ == "Square":
-                instance = cls(2)
-            else:
-                instance = cls(2, 4)
+            instance = cls(2, 4)  # create a new instance
             instance.update(**dictionary)
             return instance
 
     @classmethod
     def load_from_file(cls):
+
+        """ That returns a list of instances
+        Args:
+            cls (class): Rectangle or Square
+
+            raises:
+                IOError: return []
+
+            Return: a list of instances or []
+        """
         filename = '{}.json'.format(cls.__name__)
         try:
             with open(filename, mode='r', encoding='utf-8') as file:
@@ -70,6 +120,18 @@ class Base():
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
+
+        """ Method thats serializes in CSV
+        Args:
+            cls (class): Rectangle or Square
+
+        raises:
+            IOError: return []
+
+        Return:
+            Rectangle - <id>,<width>,<height>,<x>,<y>
+            Square - <id>,<size>,<x>,<y>
+        """
         csvfile = '{}.csv'.format(cls.__name__)
         with open(csvfile, 'w', newline='') as csvf:
             if list_objs is None or list_objs == []:
@@ -84,6 +146,18 @@ class Base():
 
     @classmethod
     def load_from_file_csv(cls):
+
+        """ Method thats deserializes in CSV
+        Args:
+            cls (class): Rectangle or Square
+
+        raises:
+            IOError: return []
+
+        Return:
+            Rectangle - <id>,<width>,<height>,<x>,<y>
+            Square - <id>,<size>,<x>,<y>
+        """
         csvfile = '{}.csv'.format(cls.__name__)
         try:
             with open(csvfile, 'r', newline='') as csvf:
@@ -92,7 +166,8 @@ class Base():
                 else:
                     fields = ["id", "width", "height", "x", "y"]
                 doc = csv.DictReader(csvf, fieldnames=fields)
-                ls_of_dics = [{k: int(v) for k, v in dic.items()} for dic in doc]
+                ls_of_dics = [{k: int(v) for k, v in dic.items()}
+                              for dic in doc]
                 return [cls.create(**dics) for dics in ls_of_dics]
         except IOError:
             return []
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,7 +9,7 @@ class Square(Rectangle):
     """ Class Square """
 
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(size, size, x, y)
+        super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
