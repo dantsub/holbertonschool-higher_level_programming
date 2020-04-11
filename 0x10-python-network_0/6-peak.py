b9e2555e41fcd1e3598c1558936b71bbcb79370a
dantsub <dany.mrtinez@gmail.com>
2020-04-11 01:35:04 +0000

    change algorithm

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,5 +8,8 @@ def find_peak(list_of_integers):
     Args:
     list_of_integers
     """
-    if list_of_integers:
+    if type(list_of_integers) == list:
+        if len(list_of_integers) == 0:
+            return None
         return max(list_of_integers)
+    return None
