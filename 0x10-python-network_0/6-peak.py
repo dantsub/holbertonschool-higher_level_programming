d0f6848935fe7afb98e741b9ad958e528de1b60c
dantsub <dany.mrtinez@gmail.com>
2020-04-11 02:03:37 +0000

    change algorithm again

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,12 +4,32 @@
 
 def find_peak(list_of_integers):
     """
-    find_peak function find a peak
+    find_peak - function find a peak
     Args:
     list_of_integers
     """
-    if type(list_of_integers) == list:
-        if len(list_of_integers) == 0:
-            return None
+    if list_of_integers == []:
+        return None
+    lenlist = len(list_of_integers)
+    if lenlist == 1:
+        return list_of_integers[0]
+    if lenlist == 2:
         return max(list_of_integers)
-    return None
+    return list_of_integers[peak(list_of_integers, 0, lenlist - 1)]
+
+
+def peak(listin, left, right):
+    """
+    peak - search the peak with recursion
+    Args:
+    listin: list of integer
+    left: left of list
+    right: right of list
+    return: position of the peak
+    """
+    if left == right:
+        return left
+    mid = int((left + right) / 2)
+    if listin[mid] > listin[mid + 1]:
+        return peak(listin, left, mid)
+    return peak(listin, mid + 1, right)
