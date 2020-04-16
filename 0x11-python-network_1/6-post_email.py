92d059e3e26602c810f2212e7fb7933732864010
dantsub <dany.mrtinez@gmail.com>
2020-04-16 00:34:46 -0500

    task 5 and 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+# POST request with requests package
+import requests
+from sys import argv as av
+
+
+if __name__ == '__main__':
+    url, data = av[1], {"email": av[2]}
+    req = requests.post(url, data=data)
+    print(req.text)
