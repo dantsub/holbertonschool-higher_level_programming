9df15d007f6c1a25df0e33ec11750ba461d77f78
dantsub <dany.mrtinez@gmail.com>
2020-05-12 17:56:39 -0500

    use recursion

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,13 +3,15 @@ const request = require('request');
 request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, response, body) => {
   if (!err) {
     const characters = JSON.parse(body).characters;
-    for (let index = 0; index < characters.length; index++) {
-      request(characters[index], (err, response, body) => {
-        console.log(characters[index]);
-        if (!err) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
+    recursionRequestPrint(characters, 0);
   }
 });
+
+function recursionRequestPrint (url, index) {
+  request(url[index], (err, response, body) => {
+    if (!err) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < url.length) recursionRequestPrint(url, ++index);
+    }
+  });
+}
