2f650aa429b537cf1acaa01b5beef208ffbb4b88
dantsub <dany.mrtinez@gmail.com>
2020-05-12 12:57:29 -0500

    change position err variable

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,5 +2,5 @@
 const request = require('request');
 request
   .get(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, response, body) => {
-    console.log(JSON.parse(body).title || err);
+    console.log(err || JSON.parse(body).title);
   });
